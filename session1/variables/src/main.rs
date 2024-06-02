36d3443c98ad57185147381d7d42df112026fe42
Vitaliy Vanyushin <wm@codehtml.ru>
2024-06-02 09:10:38 +0400

    1.4

--- session1/variables/src/main.rs
+++ session1/variables/src/main.rs
@@ -1,4 +1,50 @@
+fn double(n: i32) -> i32 {
+    n * 2
+}
+
+// fn double_or_noting(n: i32) -> i32 {
+//     if n > 0 {
+//         n * 2
+//     } else {
+//         0
+//     }
+// }
+
+fn greet(s: String) -> String {
+    println!("Hello {s}");
+    s
+}
+
+fn greet_borrow(s: &String) {
+    println!("{s}");
+}
+
+fn greet_borrow_mut(s: &mut String) {
+    *s = format!("greet_borrowMut {s}");
+}
+
 fn main() {
-    let n: i32 = 5;
+    let mut n: i32 = 5;
+    n += 1;
+
+    {
+        let n: i32 = 5;
+        let n: i32 = n + 1;
+
+        println!("{n}");
+    }
+
     println!("{n}");
+
+    let m: i32 = double(5);
+    println!("{}", m);
+
+    let mut name = "Hello".to_string();
+
+    greet(name.clone());
+    // greet(name);
+    greet_borrow(&name);
+
+    greet_borrow_mut(&mut name);
+    println!("{}", name)
 }
